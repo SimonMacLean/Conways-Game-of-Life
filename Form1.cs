23b033903dd63ed5d9c58f334967f92ad09c55cb
Simon MacLean <Simon MacLean>
2016-03-17 18:53:53 -0700

    Added a starting pattern that will be placed in middle

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,23 @@ namespace Conways_game_of_life
 {
     public partial class mainForm : Form
     {
+        static bool X = true;
+        static bool _ = false;
+        bool[][] startingPattern = new bool[][] {
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,X,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,X,_,X,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,_,X,X,_,_,_,_,_,_,X,X,_,_,_,_,_,_,_,_,_,_,_,_,X,X},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,X,_,_,_,X,_,_,_,_,X,X,_,_,_,_,_,_,_,_,_,_,_,_,X,X},
+            new bool[] {X,X,_,_,_,_,_,_,_,_,X,_,_,_,_,_,X,_,_,_,X,X,_,_,_,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {X,X,_,_,_,_,_,_,_,_,X,_,_,_,X,_,X,X,_,_,_,_,X,_,X,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,X,_,_,_,_,_,X,_,_,_,_,_,_,_,X,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,X,_,_,_,X,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_},
+            new bool[] {_,_,_,_,_,_,_,_,_,_,_,_,X,X,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_}
+        };
         Point mouse;
         bool clicking = false;
         bool changesMade = true;
-        static int zoom = 1;
+        static Size pointSize = new Size(2,2);
         Timer drawTimer = new Timer();
         bool addingCities = true;
         bool[][] aliveVals;
@@ -28,7 +41,7 @@ namespace Conways_game_of_life
             | BindingFlags.Instance | BindingFlags.NonPublic, null,               
             this, new object[] { true });
             drawTimer.Enabled = false;
-            drawTimer.Interval = 10;
+            drawTimer.Interval = 1;
             drawTimer.Tick += drawTimer_Tick;
         }
 
@@ -41,10 +54,12 @@ namespace Conways_game_of_life
             Invalidate();
             if (addingCities)
                 return;
-            bool[][] prevCities = new bool[Height / zoom][];
+            if (pointSize.Width == 0 || pointSize.Height == 0)
+                return;
+            bool[][] prevCities = new bool[Height / pointSize.Height][];
             for (int i = 0; i < prevCities.Length; i++)
             {
-                prevCities[i] = new bool[Width / zoom];
+                prevCities[i] = new bool[Width / pointSize.Width];
                 for (int j = 0; j < prevCities[i].Length; j++)
                 {
                     prevCities[i][j] = false;
@@ -75,20 +90,30 @@ namespace Conways_game_of_life
         }
         void init()
         {
-            aliveVals = new bool[Height / zoom][];
+            if (pointSize.Width == 0 || pointSize.Height == 0)
+                return;
+            aliveVals = new bool[Height / pointSize.Height][];
             for (int i = 0; i < aliveVals.Length; i++)
             {
-                aliveVals[i] = new bool[Width / zoom];
+                aliveVals[i] = new bool[Width / pointSize.Width];
                 for (int j = 0; j < aliveVals[i].Length; j++)
                 {
                     aliveVals[i][j] = false;
+                    int startY = aliveVals.Length / 2 - startingPattern.Length / 2;
+                    int endY = aliveVals.Length / 2 + startingPattern.Length / 2;
+                    int startX = aliveVals[0].Length / 2 - startingPattern[0].Length / 2;
+                    int endX = aliveVals[0].Length / 2 + startingPattern[0].Length / 2;
+                    if (i >= startY && i <= endY && j >= startX && j < endX)
+                        aliveVals[i][j] = startingPattern[i - startY][j - startX];
                 }
             }
         }
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
+            if (pointSize.Width == 0 || pointSize.Height == 0)
+                return;
             clicking = true;
-                aliveVals[e.Y / zoom][e.X / zoom] = !aliveVals[e.Y / zoom][e.X / zoom];
+                aliveVals[mouse.Y / pointSize.Height][mouse.X / pointSize.Width] = !aliveVals[mouse.Y / pointSize.Height][mouse.X / pointSize.Width];
                 update();
                 changesMade = true;
         }
@@ -96,22 +121,23 @@ namespace Conways_game_of_life
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
 
-            Rectangle bounds = Screen.PrimaryScreen.Bounds;
-            Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
-            using (Graphics g = Graphics.FromImage(bitmap))
+            //Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            //Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+            //using (Graphics g = Graphics.FromImage(bitmap))
+            //{
+            //    g.CopyFromScreen(Point.Empty, new Point(0, 5), bounds.Size);
+            //}
+            if(pointSize.Width != 0 && pointSize.Height != 0)
+                e.Graphics.FillRectangle(Brushes.Gray, mouse.X/pointSize.Width*pointSize.Width, mouse.Y / pointSize.Height * pointSize.Height, pointSize.Width, pointSize.Height);
+            if(changesMade)
             {
-                g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
-            }
-            e.Graphics.FillRectangle(Brushes.Black, mouse.X, mouse.Y, zoom, zoom);
-            if (changesMade)
-            {
-                e.Graphics.Clear(addingCities ? Color.FromArgb(254, 254, 254) : Color.White);
+                //e.Graphics.Clear(/*addingCities ? */Color.FromArgb(254, 254, 254)/* : Color.White*/);
                 for (int i = 0; i < aliveVals.Length; i++)
                 {
                     for (int j = 0; j < aliveVals[i].Length; j++)
                     {
                         if (aliveVals[i][j])
-                            e.Graphics.FillRectangle(new SolidBrush(addingCities ? Color.Black : invertColor(bitmap.GetPixel(j * zoom, i * zoom))), j * zoom, i * zoom, zoom, zoom);
+                            e.Graphics.FillRectangle(new SolidBrush(/*addingCities ? */Color.Black/* : invertColor(bitmap.GetPixel(j * zoom, i * zoom))*/), j * pointSize.Width, i * pointSize.Height, pointSize.Width, pointSize.Height);
                     }
                 }
                 changesMade = false;
@@ -120,21 +146,36 @@ namespace Conways_game_of_life
 
         private void mainForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            switch (e.KeyCode)
             {
-                clicking = false;
-                drawTimer.Enabled = !drawTimer.Enabled;
-                addingCities = !addingCities;
-                this.FormBorderStyle = !addingCities? FormBorderStyle.None : FormBorderStyle.Sizable;
+                case Keys.Enter:
+                    clicking = false;
+                    drawTimer.Enabled = !drawTimer.Enabled;
+                    addingCities = !addingCities;
+                    break;
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+                case Keys.Delete:
+                    init();
+                    break;
+                case Keys.Back:
+                    init();
+                    break;
+                case Keys.Down:
+                    pointSize.Height--;
+                    break;
+                case Keys.Up:
+                    pointSize.Height++;
+                    break;
+                case Keys.Left:
+                    pointSize.Width--;
+                    break;
+                case Keys.Right:
+                    pointSize.Width++;
+                    break;
             }
-            if (e.KeyCode == Keys.Escape)
-                Application.Exit();
-            if (e.KeyCode == Keys.Delete || e.KeyCode == Keys.Back)
-                init();
-            if (e.KeyCode == Keys.Down)
-                zoom--;
-            if (e.KeyCode == Keys.Up)
-                zoom++;
+            Invalidate();
 
         }
 
@@ -148,10 +189,13 @@ namespace Conways_game_of_life
             mouse = e.Location;
             if (clicking)
             {
-                aliveVals[e.Y / zoom][e.X / zoom] = !aliveVals[e.Y / zoom][e.X / zoom];
+                if (pointSize.Width == 0 || pointSize.Height == 0)
+                    return;
+                aliveVals[e.Y / pointSize.Width][e.X / pointSize.Height] = !aliveVals[e.Y / pointSize.Width][e.X / pointSize.Height];
                 update();
-                changesMade = true;
             }
+            changesMade = true;
+            if(addingCities)Invalidate();
         }
         public static Color invertColor(Color c)
         {
